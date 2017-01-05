0b7ff140dff51bde91795f75dd2391947d4d32fb
2532264976@qq.com <lvhaidong520@163.com>
2017-01-05 11:44:04 +0800

    维护商品admin-吕海冬

--- tiantian/tiantian/consumer/models.py
+++ tiantian/tiantian/consumer/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 class UserInfo(models.Model):
-	# 用户信息
-	uname = models.CharField(max_length=20)
-	upwd = models.CharField(max_length=20)	
-	uemail = models.CharField(max_length=40)
-	isDelete = models.BooleanField(default = False)
-	class Meta():
-        db_table = "userinfo"
+    # 用户信息
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=20)
+    uemail = models.CharField(max_length=40)
+    isDelete = models.BooleanField(default = False)
+    class Meta():
+		db_table = "userinfo"
 
 
 class RecInfo(models.Model):
-	#收件人地址信息
-	name = models.CharField(max_length=20)
-	address = models.CharField(max_length=100)
-	tel = models.CharField(max_length=20)
-	postcode = models.CharField(max_length=20)
-	isDelete = models.BooleanField(default=False)
-	userNum = models.ForeignKey('UserInfo')
-	class Meta():
+    #收件人地址信息
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    tel = models.CharField(max_length=20)
+    postcode = models.CharField(max_length=20)
+    isDelete = models.BooleanField(default=False)
+    userNum = models.ForeignKey('UserInfo')
+    class Meta():
         db_table = "recinfo"
--- tiantian/tiantian/goods/urls.py
+++ tiantian/tiantian/goods/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 import views
 
 urlpatterns=[
-    url(r'^index/$',views.index),
-    url(r'^$',views.index)
+    url(r'^index/$',views.index,name='index'),
+    url(r'^$',views.index,name='index'),
+    url(r'^list/$',views.list,name='list'),
+
 ]
