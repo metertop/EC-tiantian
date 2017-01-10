d11b321f39a951bdedeeb82ed0a4e3b62ffd3518
wanghui <wanghui9410@163.com>
2017-01-10 15:01:54 +0800

    tijiao

--- tiantian/tiantian/order/models.py
+++ tiantian/tiantian/order/models.py
@@ -6,19 +6,16 @@ class OrderInfo(models.Model):
 	user = models.CharField(max_length=20)
 	ototal = models.DecimalField(max_digits=10,decimal_places=2)
 	state = models.BooleanField(default=False)
-
+#订单详情
 class OrderDetailInfo(models.Model):
 	order = models.ForeignKey('OrderInfo')
 	goods = models.CharField(max_length=20)
 	# CommaSeparatedIntegerField逗号分隔的整数，IntegerField 整数
 	count = models.IntegerField(max_length=None)
 	price = models.DecimalField(max_digits=20,decimal_places=2)
-#购物车数据
-class CartInfo(models.Model):
-	user = models.CharField(max_length=20)
-	goods = models.CharField(max_length=20)
-	count = models.IntegerField(max_length=None)
-	isDelete = models.BooleanField(default=False)
+	tprice = models.ForeignKey('cart.CartInfo')
 
-	class Meta():
-		db_table = 'cartinfo'
+
+	
+
+	
--- tiantian/tiantian/order/views.py
+++ tiantian/tiantian/order/views.py
@@ -3,20 +3,37 @@ from django.shortcuts import render
 import MySQLdb
 from models import OrderInfo,OrderDetailInfo
 
+#提交订单页面
 def placeOrder(request):
 	#session 获取id
 	user_id = request.session.get('id')
-	#从购物车获取已选择的商品 订单
-	orderinfo = OrderInfo.objects.filter(user=user_id)
+	#订单总览：从购物车获取已选择的商品
+	orderinfo = CartInfo.objects.filter(user_id=user_id)
 	#获取订单详情
-	orderdetailsinfo = OrderDetailInfo.objects.filter(order=orderinfo)
-	context = {'orderinfo':orderinfo,'orderdetailsinfo':orderdetailsinfo}
-	return render(request,'order/place_order.html',context)
+	orderdetailsinfo = OrderDetailInfo.objects.filter(order_id=orderinfo.id)
+	#判断支付状态
+	if OrderInfo.state==True:
+		context = {'orderinfo':orderinfo,'orderdetailsinfo':orderdetailsinfo}
+		return render(request,'order/user_center_order.html',context)
 
-
+#订单总览页面
 def userCenterOrder(request):
-	return render(request,'order/user_center_order.html') 
+	#session 获取id
+	user_id = request.session.get('id')
+	#订单总览：从购物车获取已选择的商品
+	orderinfo = CartInfo.objects.filter(user_id=user_id)
+	#判断，点击去付款，返回提交订单页面
+	if OrderInfo.state==False:
+		context = {'orderinfo':orderinfo}
+		#返回的提交订单页面
+		return render(request,'order/place_order.html',context) 
 
+#订单详情页面
 def orderDetailsInfo(request):
-	return render(request,'order/order_details_info.html')
+	#session 获取id
+	user_id = request.session.get('id')
+	#获取订单详情
+	orderdetailsinfo = OrderDetailInfo.objects.filter(order_id=orderinfo.id)
+	context = {'orderdetailsinfo':orderdetailsinfo}
+	return render(request,'order/order_details_info.html',context)
 
